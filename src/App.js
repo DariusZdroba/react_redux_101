ada225857445f5301de9ae2fdd31bdc5a1fe12ed
Zdroba Darius <dariuszdroba@gmail.com>
2023-09-21 17:15:24 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useSelector, useDispatch} from 'react-redux';
+import './App.css';
+import { actions } from './store/index';
+function App() {
+  const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment())
+  }
+  const decrement = () => {
+    dispatch(actions.decrement())
+  }
+  return (
+    <div className="App">
+      <h1>counter app</h1>
+      <h2>{ counter} </h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
