d453ee392d0691ec3577c0bc7eefa7e38ccdf392
Zdroba Darius <dariuszdroba@gmail.com>
2023-09-21 17:26:06 +0300

    added commentary

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { useSelector, useDispatch} from 'react-redux';
-import './App.css';
-import { actions } from './store/index';
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { actions } from "./store/index";
 function App() {
-  const counter = useSelector((state) => state.counter)
+  const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
   const increment = () => {
-    dispatch(actions.increment())
-  }
+    dispatch(actions.increment());
+  };
   const decrement = () => {
-    dispatch(actions.decrement())
-  }
+    dispatch(actions.decrement());
+  };
   return (
     <div className="App">
+      {/* Simple counter eleent using state */}
       <h1>counter app</h1>
-      <h2>{ counter} </h2>
+      <h2>{counter} </h2>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
